92c4d2f338ab13c9a6e56d8cebbe20042cb98fe3
William Anderson <114448677+wllmAnderson@users.noreply.github.com>
2023-10-08 12:24:26 -0400

    update footer

--- ppcc/src/App.js
+++ ppcc/src/App.js
@@ -19,9 +19,9 @@ function App() {
   </div>
 </div>
     </body>
-    <div class="attribution">
+    <div class="attribution position-absolute bottom-0 start-50 translate-middle-x">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/wllmAnderson">William Anderson</a>.
       </div>
       </>
   );
