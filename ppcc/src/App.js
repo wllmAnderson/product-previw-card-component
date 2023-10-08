6f5848d26061bd74a448ace51e05e96c6218035b
William Anderson <114448677+wllmAnderson@users.noreply.github.com>
2023-10-08 11:40:48 -0400

    update text fix margens

--- ppcc/src/App.js
+++ ppcc/src/App.js
@@ -6,30 +6,23 @@ import Text from './Text';
 
 function App() {
   return (
-    <body>
-<Text />
-      
-      
-      <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
-      </div>
-      <div class="card mb-3" style={{maxWidth: '540px'}}>
+    <>
+    <body>      
+      <div className="card mb-3 rounded fixed-top position-absolute top-50 start-50 translate-middle"
+        style={{ maxWidth: "540px" }}>
   <div class="row g-0">
     <div class="col-md-4">
       <Image />
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
+    <Text />
   </div>
 </div>
-
     </body>
+    <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+      </>
   );
 }
 
--- ppcc/src/Text.js
+++ ppcc/src/Text.js
@@ -1,13 +1,18 @@
-
+import '/Users/williamanderson/Desktop/Github-Repo/product-previw-card-component/ppcc/src/Text.css'
 const Text = () => {
   return (
     <>
-<div class="col-md-8">
+<div class="col-md-8 p-4">
       <div class="card-body">
-        <h5 class="card-title">Perfume</h5>
-        <p class="card-text">Gabrielle Essence Eau De Parfum</p>
-        <p class="card-text"><small class="text-body-secondary">A floral, solar and voluptuous interpretation composed by Olivier Polge, 
-      Perfumer-Creator for the House of CHANEL.</small></p>
+        <h5 class="card-title fs-6">Perfume</h5>
+        <p class="card-text fs-3">Gabrielle<br></br> Essence Eau<br></br> De Parfum</p>
+        <p class="card-text fs-6" style={{ fontFamily: 'Fraunces, serif' }}>
+                <small class="text-body-secondary text-wrap" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+                  A floral, solar and voluptuous<br></br> interpretation
+                  composed by Olivier Polge, Perfumer-Creator for the House of
+                  CHANEL.
+                </small>
+              </p>
       <p class="text-success fs-3">
                 $149.99{" "}
                 <span class="text-decoration-line-through text-black fs-6">
