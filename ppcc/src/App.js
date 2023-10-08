bb02c22a03015284de8fc5c6af38991f60a87a8e
William Anderson <114448677+wllmAnderson@users.noreply.github.com>
2023-10-08 09:03:49 -0400

    add images

--- ppcc/src/App.js
+++ ppcc/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+
+      Perfume
+
+      Gabrielle Essence Eau De Parfum
+
+      A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+      Perfumer-Creator for the House of CHANEL.
+
+      $149.99
+      $169.99
+
+      Add to Cart
+      
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+      <div class="card mb-3" style={{maxWidth: '540px'}}>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="" class="img-fluid rounded-start" alt="..." />
     </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </body>
   );
 }
 
